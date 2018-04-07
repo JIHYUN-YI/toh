e787d888776fe27daf833e71c9a6ce3cadc1427d
이문용 <matinlmy@naver.com>
2018-04-07 17:34:58 +0900

    service DI

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../HEROES';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -16,16 +17,23 @@ export class HeroesComponent implements OnInit {
 
   // using 'new' keyword (생성자 함수)
   // hero = new Hero();
-
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  // 2) 서비스를 생성자로 주입
+  constructor(private heroService: HeroService) {
     /*this.hero.id = 1;
     this.hero.name = 'Windstorm';*/
   }
 
   ngOnInit() {
+    // 서비스 호출
+    // 1) 잘못된 방법: 의존성이 생긴다. 컴포넌트가 서비스를 제어하게 되므로.
+    /*const heroService = new HeroService();
+    this.heroes = heroService.getHeroes();*/
+    this.heroes = this.heroService.getHeroes();
+
+
   }
 
   onSelect(hero: Hero) {
